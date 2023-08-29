b33cfb6bd593c57d520eefd5070e15c806bbc51f
Callu <jatcee2@gmail.com>
2023-08-29 18:43:31 +1200

    New

--- app/src/main/java/com/example/stepcountingapp/MainActivity.java
+++ app/src/main/java/com/example/stepcountingapp/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager manager;
     private float totalSteps = 0f;
     private float previousTotalSteps = 0f;
@@ -20,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadData();
+        resetSteps();
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = manager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(sensor == null)
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show();
+        else {
+            manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+
+        }
+
     }
 
     private void saveData() {
@@ -50,8 +67,39 @@ public class MainActivity extends AppCompatActivity {
                 previousTotalSteps = totalSteps;
                 steps.setText("0");
                 saveData();
+                Toast.makeText(getApplicationContext(), "Steps reset", Toast.LENGTH_SHORT).show();
+
                 return true;
             }
         });
+    }
+
+
+    private void reset(View view){
+        TextView steps = findViewById(R.id.tv_stepsTaken);
+        Button reset = findViewById(R.id.button);
+
+        previousTotalSteps = totalSteps;
+        steps.setText("0");
+        saveData();
+        Toast.makeText(getApplicationContext(), "Steps reset", Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView tv_stepsTaken = findViewById(R.id.tv_stepsTaken);
+        totalSteps = event.values[0];
+        // Current steps are calculated by taking the difference of total steps
+        // and previous steps
+        int currentSteps = (int) totalSteps - (int) previousTotalSteps;
+
+        // It will show the current steps to the user
+        tv_stepsTaken.setText(String.valueOf(currentSteps));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
